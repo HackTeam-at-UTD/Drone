4e65ee21c679dcccc401963f41a53e7c5b6b137f
Shaurya Dwivedi <67282772+Shaurya0108@users.noreply.github.com>
2023-04-16 00:53:21 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,3 +16,34 @@ const App = () => {
 }
 
 export default App;
+
+// import React, {useState, useEffect} from 'react'
+
+// function App() {
+//   const [data, setData] = useState([{}])
+
+//   useEffect(() => {
+//     fetch("/numberOfWaterBottles").then(
+//       res => res.json()
+//     ).then(
+//       data => {
+//         setData(data)
+//         console.log(data)
+//       }
+//   )
+// }, [])
+
+//   return (
+//     <div>
+//       {(typeof data.numberOfWaterBottles == 'undefined') ? (
+//         <p>loading...</p>
+//       ):(
+//         data.numberOfWaterBottles.map((numberOfWaterBottle, i) => (
+//           <p key={i}> {numberOfWaterBottle} </p>
+//         ))
+//       )}
+//     </div>
+//   )
+// }
+
+// export default App
