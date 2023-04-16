28f4dfb38ad58eab40248b42a7e2b2083f9e47fa
Shaurya Dwivedi <67282772+Shaurya0108@users.noreply.github.com>
2023-04-15 22:18:22 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Items from './Items_list.js';
 import Home from './Home.js';
-// import NotFound from '../pages/NotFound'
-
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/items" element={<Items/>}/>
         </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
